37eb88dde15f38309503bb9b4b2ec001442fb6fa
花儿1 <530488343@qq.com>
2023-03-04 12:01:53 +0800

    feat:add getCurrentInstance Api

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -38,10 +38,11 @@ function setupStatefulComponent(instance) {
   const { setup } = Component
 
   if (setup) {
-    
+    setCurrentInstance(instance)
     const setupResult = setup(shallowReadonly(instance.props),{
       emit:instance.emit,
     })
+    setCurrentInstance(null)
 
     handleSetupResult(instance, setupResult)
   }
@@ -65,4 +66,14 @@ function finishComponentSetup(instance) {
   // if(Component.render) {
   instance.render = Component.render
   // }
+}
+
+let currentInstance = null
+
+export function getCurrentInstance() {
+  return currentInstance
+}
+
+export function setCurrentInstance(instance) {
+  currentInstance = instance
 }
