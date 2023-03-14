ff645d52742bc6270064f9d5ac21f9d6292fcabc
Michelangelo Lv <mgaligigi@163.com>
2023-03-14 16:24:30 +0800

    feat: parseText

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -25,36 +25,58 @@ function parseChildren(context) {
   } else if (s[0] === "<") {
     // 如果第一个字符是<,说明是一个标签
     if (/[a-z]/i.test(s[1])) {
-      console.log("parse element");
+      // console.log("parse element");
       node = parseElement(context);
     }
   }
-  // 推入nodes
+
+  if (!node) {
+    console.log("parse text");
+    node = parseText(context);
+  }
+  // 推入node
   nodes.push(node);
   // 返回ast树
   return nodes;
 }
 
+function parseText(context: any) {
+  // 1. 获取content
+  const content = parseTextData(context, context.source.length);
+  // 推进清空所有 source
+  console.log("-----", context.source);
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
+
+//获取content,并且推进content的距离
+function parseTextData(context: any, length) {
+  const content = context.source.slice(0, length);
+  advanceBy(context, length);
+  return content;
+}
+
 // 解析Element
 function parseElement(context: any) {
-    // Implement
+  // Implement
   // 1. 解析tag
   const element = parseTag(context, TagType.Start);
 
   parseTag(context, TagType.End);
-  console.log("-------", context.source);
-
-
+  // console.log("-------", context.source);
   return element;
 }
 
 // 解析标签生成ast树
 function parseTag(context: any, type: TagType) {
-    // 正则匹配context.source 是 <div 或者 </div
+  // 正则匹配context.source 是 <div 或者 </div
   const match: any = /^<\/?([a-z]*)/i.exec(context.source);
   // console.log(match);
   // 拿到匹配的tag放入ast树
-  const tag:any = match[1];
+  const tag: any = match[1];
   // 2. 删除处理完成的代码
   advanceBy(context, match[0].length);
   advanceBy(context, 1);
@@ -83,13 +105,13 @@ function parseInterpolation(context) {
   // context去掉{{
   advanceBy(context, openDelimiter.length);
   //   context去掉}}后的内容
-  const rawContent = context.source.slice(0, rawContentLength);
+  const rawContent = parseTextData(context, rawContentLength);
   // 给context去掉所有空格
   const content = rawContent.trim();
 
   // 给context.source清空
   // 因为后续还要处理div等标签内容，所以清空等待下个逻辑进入
-  advanceBy(context, rawContentLength + closeDelimiter.length);
+  advanceBy(context, closeDelimiter.length);
 
   return {
     type: NodeTypes.INTERPOLATION,
@@ -100,7 +122,7 @@ function parseInterpolation(context) {
   };
 }
 
-// 字符串缩小，根据传入的length，截取并重新覆盖自身
+// 推进：字符串越来越缩小，根据传入的length，推进并重新覆盖自身
 function advanceBy(context, length: number) {
   context.source = context.source.slice(length);
 }
